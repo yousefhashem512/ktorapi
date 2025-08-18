ff483fc8ecc8f3b66c0c3e23620f0d3e387373ef
Yousef <yousefhashem512@gmail.com>
2025-08-19 02:03:29 +0300

    nitial commit

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -4,9 +4,6 @@ import com.yousefh.database.MongoConnection
 import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
-    val database = MongoConnection.getDatabase()
-    println("Database connected: ${database.name}")
-
     io.ktor.server.netty.EngineMain.main(args)
 }
 
--- src/main/kotlin/database/MongoConnection.kt
+++ src/main/kotlin/database/MongoConnection.kt
@@ -9,6 +9,15 @@ object MongoConnection {
     private val client = KMongo.createClient(CONNECTION_STRING)
 
     fun getDatabase(): MongoDatabase {
-        return client.getDatabase("myDatabase") // اسم الداتابيز اللي هتستخدمه
+        return client.getDatabase("ReZoAppDB")
     }
 }
+
+/*
+* val client = MongoClients.create("mongodb+srv://yousefhashem128:QeZnUMyTI5m3xcCC@rezoapp.yszijb3.mongodb.net/?retryWrites=true&w=majority&appName=ReZoApp")
+    val database = client.getDatabase("ReZoAppDB")
+    val collection = database.getCollection("users")
+
+    // إدخال تجريبي
+    collection.insertOne(Document("name", "testUseranayousd"))
+    * */
